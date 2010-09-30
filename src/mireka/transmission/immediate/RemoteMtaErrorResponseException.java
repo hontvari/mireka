d02d6161955d0bd9b9950a511150068baaf449a9
Hontvári Levente <hontvari@flyordie.com>
2010-09-30 12:25:49 +0000

    logged information was redundant
    git-svn-id: http://mireka.googlecode.com/svn/trunk@52 741d60a8-641f-389e-c7ec-093451e00b49

--- src/mireka/transmission/immediate/RemoteMtaErrorResponseException.java
+++ src/mireka/transmission/immediate/RemoteMtaErrorResponseException.java
@@ -37,9 +37,4 @@ public class RemoteMtaErrorResponseException extends SendException {
     public MailSystemStatus remoteMtaStatus() {
         return remoteMtaStatus;
     }
-
-    @Override
-    public String toString() {
-        return super.toString() + " " + remoteMtaStatus;
-    }
 }
