63d1d0d7331c0b6ca8559efaa8f50f00ccf6f58c
Hontvári Levente <hontvari@flyordie.com>
2011-07-19 13:18:31 +0000

    The new AcceptRecipient filter can accept a recipient based on a RecipientSpecification.
    git-svn-id: http://mireka.googlecode.com/svn/trunk@110 741d60a8-641f-389e-c7ec-093451e00b49

---
+++ src/mireka/filter/local/AcceptRecipient.java
@@ -0,0 +1,37 @@
+package mireka.filter.local;
+
+import mireka.filter.FilterReply;
+import mireka.filter.RecipientContext;
+import mireka.filter.StatelessFilterType;
+import mireka.filter.local.table.RecipientSpecification;
+
+import org.subethamail.smtp.RejectException;
+
+public class AcceptRecipient extends StatelessFilterType {
+    private RecipientSpecification recipientSpecification;
+
+    @Override
+    public FilterReply verifyRecipient(RecipientContext recipientContext)
+            throws RejectException {
+        if (recipientSpecification.isSatisfiedBy(recipientContext.recipient))
+            return FilterReply.ACCEPT;
+        else
+            return FilterReply.NEUTRAL;
+    }
+
+    /**
+     * @category GETSET
+     */
+    public RecipientSpecification getRecipientSpecification() {
+        return recipientSpecification;
+    }
+
+    /**
+     * @category GETSET
+     */
+    public void setRecipientSpecification(
+            RecipientSpecification recipientSpecification) {
+        this.recipientSpecification = recipientSpecification;
+    }
+
+}
