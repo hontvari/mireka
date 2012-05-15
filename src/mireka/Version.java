f1098bc206672dffa0613060ca5e18e13fa367f1
Hontvári Levente <hontvari@flyordie.com>
2012-05-15 15:17:37 +0000

    version information is loaded from the manifest file of the jar instead of a separate properties file
    git-svn-id: http://mireka.googlecode.com/svn/trunk@127 741d60a8-641f-389e-c7ec-093451e00b49

--- src/mireka/Version.java
+++ src/mireka/Version.java
@@ -1,37 +1,11 @@
 package mireka;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import javax.annotation.concurrent.GuardedBy;
-
 /**
  * Version retrieves the current Mireka version number.
  */
 public class Version {
-    @GuardedBy("Version.class")
-    private static String cachedVersion;
 
     public static String getVersion() {
-        // does not work (in Resin only?)
-        // getClass().getPackage().getImplementationVersion()
-        synchronized (Version.class) {
-            if (cachedVersion == null) {
-                try {
-                    InputStream resourceAsStream =
-                            Version.class
-                                    .getResourceAsStream("/version.properties");
-                    Properties properties = new Properties();
-                    properties.load(resourceAsStream);
-                    resourceAsStream.close();
-                    cachedVersion = properties.getProperty("version");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return cachedVersion;
-        }
+        return Version.class.getPackage().getImplementationVersion();
     }
-
 }
