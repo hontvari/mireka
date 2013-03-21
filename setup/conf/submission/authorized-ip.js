4c89548f978afb02c465b7a65a53cc16ac630be4
Hontvári Levente <hontvari@flyordie.com>
2013-03-21 17:17:28 +0000

    configuration: loopback addresses are included in the list of IP addresses from which clients can bypass password authentication
    git-svn-id: http://mireka.googlecode.com/svn/trunk@176 741d60a8-641f-389e-c7ec-093451e00b49

--- setup/conf/submission/authorized-ip.js
+++ setup/conf/submission/authorized-ip.js
@@ -12,4 +12,9 @@
 	new IpAddress("192.0.2.128"),
 	new IpAddress("192.0.2.0/28")
 	*/
+	
+	// IPv4 loopback range
+	new IpAddress("127.0.0.0/8"),
+	// IPv6 loopback address
+	new IpAddress("::1"),	
 ]
