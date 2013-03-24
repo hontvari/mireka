2b49fc2cf980ea1e75dba9dc1dc3e1a3d7bf6ff5
Hontvári Levente <hontvari@flyordie.com>
2013-03-24 19:10:16 +0000

    Add comment, that stresses that before enabling JMX, documentation must be read to avoid security issues.
    git-svn-id: http://mireka.googlecode.com/svn/trunk@180 741d60a8-641f-389e-c7ec-093451e00b49

--- setup/doc/examples/daemon/default.sh
+++ setup/doc/examples/daemon/default.sh
@@ -8,6 +8,6 @@
 # You will then be able to use a java debugger on port 8000.
 #JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,address=localhost:8000,suspend=n"
 
-# enable JMX
+# Enable JMX. Read the documentation before uncommenting the next line!
 #JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.port=4452 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 
